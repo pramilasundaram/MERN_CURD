a74267aa765abd889c7f076d527bd28f19a181b6
pramila <pramilaworld@gmail.com>
2023-07-09 22:26:14 -0700

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const cors=require ("cors");
 const dotenv=require("dotenv").config();        
 const main=require('./db/dbconfig');
-const PORT=5000;   
+const PORT=process.env.PORT||5000;   
 
 main();
 
